08171fb9a64cb93c3890700ac57f097699ddc053
Ivan Radoš <irados@autozubak.hr>
2024-04-24 19:18:27 +0200

    Added new ProductCategory Table

--- WebShop_OL-OASP_DEV_H-06_23/Models/Dbo/CompanyModels/Company.cs
+++ WebShop_OL-OASP_DEV_H-06_23/Models/Dbo/CompanyModels/Company.cs
@@ -2,6 +2,7 @@
 using Shared_OL_OASP_DEV_H_06_23.Models.Base.CompanyModels;
 using System.ComponentModel.DataAnnotations;
 using WebShop_OL_OASP_DEV_H_06_23.Models.Dbo.Common;
+using WebShop_OL_OASP_DEV_H_06_23.Models.Dbo.ProductModels;
 
 namespace WebShop_OL_OASP_DEV_H_06_23.Models.Dbo.CompanyModels
 {
@@ -14,5 +15,6 @@ namespace WebShop_OL_OASP_DEV_H_06_23.Models.Dbo.CompanyModels
         public bool Valid { get; set; }
         public Address? Address { get; set; }
         public long? AddressId { get; set; }
+        public ICollection<ProductCategory>? ProductCategorys { get; set; }
     }
 }
