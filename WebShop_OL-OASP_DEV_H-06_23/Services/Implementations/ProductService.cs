e85483aba7e8d3e4ab5ab454a587aa838f80aa37
Ivan Radoš <irados@autozubak.hr>
2024-04-24 19:23:56 +0200

    update

---
+++ WebShop_OL-OASP_DEV_H-06_23/Services/Implementations/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WebShop_OL_OASP_DEV_H_06_23.Data;
+
+namespace WebShop_OL_OASP_DEV_H_06_23.Services.Implementations
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext db;
+        private readonly IMapper mapper;
+
+        public ProductService(ApplicationDbContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+
+    }
+}
