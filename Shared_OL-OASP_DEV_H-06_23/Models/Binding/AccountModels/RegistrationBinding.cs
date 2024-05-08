419c1490874b5219bc895976c6dd80421a9327c7
Ivan Radoš <irados@autozubak.hr>
2024-05-08 20:11:06 +0200

    update

---
+++ Shared_OL-OASP_DEV_H-06_23/Models/Binding/AccountModels/RegistrationBinding.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared_OL_OASP_DEV_H_06_23.Models.Binding.AccountModels
+{
+    public class RegistrationBinding
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- WebShop_OL-OASP_DEV_H-06_23/Controllers/HomeController.cs
+++ WebShop_OL-OASP_DEV_H-06_23/Controllers/HomeController.cs
@@ -1,27 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebShop_OL_OASP_DEV_H_06_23.Models;
+using WebShop_OL_OASP_DEV_H_06_23.Services.Interfaces;
 
 namespace WebShop_OL_OASP_DEV_H_06_23.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = await _productService.GetProductCategories();
+            return View(model);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
